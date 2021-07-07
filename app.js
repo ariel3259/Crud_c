7440d7cd4b01fdb949bd4a9d90187d09e70a0714
ariel3259 <susanaarcia48@gmail.com>
2021-07-06 21:05:02 -0300

    se agregaron funciones para encapsular las peticiones

--- app.js
+++ app.js
@@ -66,14 +66,12 @@ function mostrarUnCuestionario(id){
 //Editar un cuestionario.Remplazar data por  [descripcion,fechaCreacion,usuarioCreador,idcuestionario] en caso de que la modificacion no funcione
 function modificarUnCuestionario(id,desc,fecha,usuario){
 	app.put(`/api/cuestionarios/modify/:${id}`,(res,req)=>{
-		const data=[
-			descripcion=desc,
-			fechaCreacion=fecha,
-			usuarioCreador=usuario,
-			id=id
-		];
-			
-		
+		const data={
+			descripcion:desc,
+			fechaCreacion:fecha,
+			usuarioCreador:usuario,
+			id:id
+		};
 		con.query('update cuestionarios set descripcion=?,fechaCreacion=?,usuarioCreador=? where idcuestionario=?',data,(err,result)=>{
 			if(err)throw err;
 			res.send(result);
